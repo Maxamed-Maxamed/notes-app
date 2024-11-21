2fba43aefed047e2731d2b8b7cb21db339a4e0c1
Maxamed-Maxamed <20088349@mail.wit.ie>
2024-11-21 18:11:30 +0000

    Fix menu loop implementation and add basic note operation functions

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,62 @@
 package org.example
 
+import java.lang.System.exit
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+fun main() {
+    mainMenu()
+    runMenu()
+}
+
+fun mainMenu() : Int {
+  println("")
+  println("------------------------")
+  println("Note Keeper Application")
+  println("------------------------")
+  println("")
+    println("---Note Menu---")
+  println("1). Add Note")
+  println("2). View Note")
+  println("3). Edit Note")
+  println("4). Delete Note")
+  println("0). ExitApp")
+  println("========================>")
+    return readlnOrNull()?.toIntOrNull() ?: -1 // Return -1 if the input is not a valid integer
+}
+
+fun runMenu() {
+    do {
+        val option = mainMenu()
+        when (option) {
+            1 -> addNote()
+            2 -> viewNote()
+            3 -> editNote()
+            4 -> deleteNote()
+            0 -> exitApp()
+            else -> println("Invalid option" + option)
+        }
+
     }
+    while (true)
+
+}
+
+fun addNote() {
+    println("Add a note")
+}
+
+fun viewNote() {
+    println("View a note")
+}
+fun editNote() {
+    println("Edit a note")
+}
+fun deleteNote() {
+    println("Delete a note")
+}
+fun exitApp() {
+    println("Exit App BYE BYE")
+    exit(0)
 }
