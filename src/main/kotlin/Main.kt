87ad3fe1463afa89bd28e951bf572c038c837c1e
Maxamed-Maxamed <20088349@mail.wit.ie>
2024-11-21 18:24:09 +0000

    Write skeleton menu for Notes App

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -11,35 +11,40 @@ fun main() {
 }
 
 fun mainMenu() : Int {
-  println("")
-  println("------------------------")
-  println("Note Keeper Application")
-  println("------------------------")
-  println("")
-    println("---Note Menu---")
-  println("1). Add Note")
-  println("2). View Note")
-  println("3). Edit Note")
-  println("4). Delete Note")
-  println("0). ExitApp")
-  println("========================>")
-    return readlnOrNull()?.toIntOrNull() ?: -1 // Return -1 if the input is not a valid integer
+    print("""
+          ----------------------------------
+          |        NOTE KEEPER APP         |
+          ----------------------------------
+          | NOTE MENU                      |
+          |   1) Add a note                |
+          |   2) List all notes            |
+          |   3) Update a note             |
+          |   4) Delete a note             |
+          ----------------------------------
+          |   0) Exit                      |
+          ----------------------------------
+          ==>> """) // Read the user's input as a string and convert it to an integer value using the toIntOrNull() function of the readlnOrNull() function.
+    // Read the user's input as a string and convert it to an integer
+//    return readlnOrNull()?.toIntOrNull() ?: -1 // Return -1 if the input is not a valid integer
+
+    return readln().toInt() // Read the user's input as a string and convert it to an integer value using the toIntOrNull() function of the readlnOrNull() function.
 }
 
 fun runMenu() {
     do {
         val option = mainMenu()
         when (option) {
-            1 -> addNote()
-            2 -> viewNote()
-            3 -> editNote()
-            4 -> deleteNote()
-            0 -> exitApp()
-            else -> println("Invalid option" + option)
+            1 -> addNote() // handle add the note menu option here - use the addNote() function
+            2 -> viewNote() // handle view the note menu option here - use the viewNote() function
+            3 -> editNote() // handle edit the note menu option here - use the editNote() function
+            4 -> deleteNote() // handle delete the note menu option here - use the deleteNote() function
+            0 -> exitApp() // handle exit the app menu option here - use the exitApp() function
+//            else -> println("Invalid option" + option) // handle invalid option here - print a message
+        else -> println("Invalid option $option") // handle invalid option here - print a message
         }
 
     }
-    while (true)
+    while (true) // repeat the menu until the user exits the app - use a do-while loop and a while loop with a condition that is always true
 
 }
 
